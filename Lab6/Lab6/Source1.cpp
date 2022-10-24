7f9a6692dd19774841d92f7602b22d1bcb486d1c
bequiet0 <115078284+bequiet0@users.noreply.github.com>
2022-10-24 21:48:32 +0300

    5-6

---
+++ Lab6/Lab6/Source1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+    const int a = 15;
+    int A[a] = { 5, 16, 67, 34, 5, 14, 15, 67, 34, 10, 15, 67, 64, 52, 65 };
+    float fMedian = 0.0;
+
+
+    for (int i = 0; i != a; i++)
+        fMedian += A[i];
+    fMedian /= a;
+
+    cout << "Median = " << fMedian << "\r\nB[]=\r\n";
+    float B[a];
+
+    for (int i = 0; i != a; i++)
+    {
+        B[i] = A[i] - fMedian;
+        cout << B[i] << "\r\n";
+    }
+
+    cout << "\r\nPress Enter To Exit...";
+    getchar();
+    return 0;
+}
--- lab5/lab5__2/Source2.cpp
+++ lab5/lab5__2/Source2.cpp
@@ -1,67 +1,62 @@
 #include <iostream>
 #include <cmath>
 #include <Windows.h>
-#include <iomanip>
-
 using namespace std;
 
 int main()
 {
-    SetConsoleOutputCP(1251);
-    SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
 
-    long long int n = 0;
-    int k = 0, z = 1;
-    long double x, a, b, h, Y, an, S;
+	long double m, Y, x, S = 0.0, b, h, a, s0;
+	long long int n = 0;
 
-    cout << "Введите a:";
-    cin >> a;
-    cout << "Введите b:";
-    cin >> b;
-    h = (b - a) / n;
-    if (b >= a)
-    {
-        long double t = b;
-        b = a;
-        a = t;
-    }
-    cout << "Введите кол-во шагов между a и b:" << endl;
-    cin >> n;
-    if (n < 0)
-    {
-        n = -n;
-        cout << "Количество шагов не может быть отрицательным, заменим ваше значение на противоположное по знаку:" << n << endl;
+	cout << "Введите а: " << endl;
+	cin >> a;
+	cout << "Введите b: " << endl;
+	cin >> b;
+m:	cout << "Введите h: " << endl;
+	cin >> h;
 
-    }
+	if (a > b)
+	{
+		int t = a;
+		a = b;
+		b = t;
+		cout << "a и b поменяли.\n";
+	}
 
-    h = (b - a) / n;
+	if (h > (b - a))
+	{
+		cout << "h должно быть меньше b - a" << endl;
+		goto m;
+	}
 
-    if (h > (b - a))
-    {
-        cout << "введите значение a и b заново" << endl;
-    }
-    
-    cout << "x\tS(x)\tY(x)\tY(x)-S(x)\n"; 
+	cout << "Кол-во шагов: ";
+	cin >> n;
 
-    for (x = a; x <= b; x = x + h)
-    {
-        Y = ((1 + pow(x, 2)) / 2) * 1.0 / tan(x) - x / 2;
-        an = a * n;
-        S = a * n;
-        for (int i = 1; i < n; i++)
-        {
-            for (i = 1; i < n; i++)
-            {
-                z *= i;
-            }
-            an = an * (pow(x, 2 * k + 1)) / 4 * pow(k, 2) - 1;
-        }
-        cout << setprecision(2) << fixed << x << "\t" << setprecision(6) << Y << " " << S << "\t";
-        cout << "S(x) = " << S;
-        cout << "Y(x) = " << Y;
-       
-    }
+	if (n < 0)
+	{
+		n = -n;
+		cout << "Не отрицательное число шагов" << endl;
+	}
 
-    
+	cout << "|\tx\t|\tS(x)\t|\tY(x)\t|\S(x)-Y(x)|\t|\n";
 
+	for (x = a; x <= b; x = x + h)
+	{
+		for (int k = 1; k < n + 1; k++)
+		{
+			s0 = ((pow(-1, k + 1)) * ((pow(x, 2 * k + 1))) / (4 * pow(k, 2) - 1));
+			S = S + s0;
+		}
+		Y = ((1 + pow(x, 2)) / 2) * atan(x) - x / 2;
+		m = abs(S - Y);
+		cout << "|\t" << x << "\t|\t" << S << "\t|\t" << Y << "\t|\t" << m << "\t|\n";
+		S = 0;
+	}
+
+
+	system("pause");
+	return 0;
 }
